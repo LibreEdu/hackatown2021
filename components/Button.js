9b689e57506cbb69b65ce5a728672243e24c22fb
Maël LE PETIT <makibeardy@gmail.com>
2021-02-05 21:32:54 -0500

    LoginView & Setup env.

---
+++ components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Button as PaperButton } from 'react-native-paper'
+import { Theme } from '../styles/Theme'
+
+const Button = ({ mode, style, ...props }) => (
+    <PaperButton
+        style={[
+            styles.button,
+            mode === 'outlined' && { backgroundColor: Theme.colors.surface },
+            style,
+        ]}
+        labelStyle={styles.text}
+        mode={mode}
+        {...props}
+    />
+)
+
+const styles = StyleSheet.create({
+    button: {
+        width: '100%',
+        marginVertical: 10,
+        paddingVertical: 2,
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 15,
+        lineHeight: 26,
+    },
+})
+
+export default Button
